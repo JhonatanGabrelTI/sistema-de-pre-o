f9a78d934acff83816d0658989d0b192ee3af782
Jhonatan <jhonatan@example.com>
2026-03-11 12:58:40 -0300

    Fix: Clean monorepo structure for Vercel

--- api/index.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-# Adiciona o diretório atual ao path para que o import da 'app' funcione localmente
-sys.path.append(os.path.dirname(__file__))
-
-from app.main import app as handler # Vercel uses the exported object
-
-app = handler # Export as 'app' as well just in case
